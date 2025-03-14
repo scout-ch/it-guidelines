57558c4104e0d94725f39c33fe0cb4a441f759ff
Diego Steiner <diego.steiner@u041.ch>
2025-03-14 19:55:52 +0000

    fix: ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
-    svgr()
+    svgr(),
+    tailwindcss()
   ]
 })
